51ef265735b6b109ea5d5d4692be296aa036f606
Anderson Alves <contatodeanderson@gmail.com>
2023-07-29 22:39:13 -0300

    added Client entity

---
+++ src/main/java/com/devsuperior/crud/domain/entity/Client.java
@@ -0,0 +1,38 @@
+package com.devsuperior.crud.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private Double income;
+
+    private Instant birthDate;
+
+    private Integer children;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anderson', '01234567890', 5000.0, '1982-07-08 12:34:56', 2);
